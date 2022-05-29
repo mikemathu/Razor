d8a68a8bdf3d994f18e002406c92aecbf171d77a
Michael Mathu <mathumichael17@gmail.com>
2022-05-29 16:56:17 +0300

    using view imports

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Razor.Models;
+using System.Diagnostics;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ViewResult Index()
+        {
+            Product myProduct = new Product
+            {
+                //Populating of Product object
+                ProductID = 1,
+                Name = "Kayak",
+                Description = "A boat for one person",
+                Category = "Watersports",
+                Price = 275M
+            };
+
+            return View(myProduct); //passed product to the view
+        }
+
+    }
+}
